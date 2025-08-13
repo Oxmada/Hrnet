6d26956db68ada9caf59c898d85e46ed15b7cb0f
Antoine Ponthieu <antoineponthieu@hotmail.fr>
2025-08-13 14:54:27 +0300

    Initialisation du projet

---
+++ src/router/router.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+
+
+function Router() {
+    return (
+        <Routes>
+            <Route>
+                <Route path="/" />
+            </Route>
+        </Routes>
+    )
+}
+
+export default Router;
